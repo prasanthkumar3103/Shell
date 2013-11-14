8c358b9a6b5e7a331dec6912c8bfbb95c32f0166
Leo G <leo@leog.in>
2013-11-15 03:01:39 +0530

    menu license

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -x
 #Author Leo G
+#Copyright 2013, Leonard Gonsalves leog.in leo@leog.in
+#  This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #Just add the name of your server in the server variable below for eg: if your server name is hostname1.com then add server1=hostname1.com
 
 server1=""
